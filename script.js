7c0a2aa3b61143dceb23fef8610bc23005ce60bb
rheajhencabanayanprod <cabanayanrheajhen@gmail.com>
2026-07-15 09:09:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Flower loaded!");
+window.onload = function () {
+
+    const flower = document.querySelector(".flower");
+    const title = document.querySelector("h1");
+    const message = document.querySelector(".message");
+
+    flower.style.transform = "scale(0)";
+    flower.style.opacity = "0";
+
+    setTimeout(() => {
+        flower.style.transition = "2s";
+        flower.style.transform = "scale(1)";
+        flower.style.opacity = "1";
+    }, 500);
+
+    title.style.opacity = "0";
+    message.style.opacity = "0";
+
+    setTimeout(() => {
+        title.style.transition = "1.5s";
+        title.style.opacity = "1";
+    }, 2500);
+
+    setTimeout(() => {
+        message.style.transition = "2s";
+        message.style.opacity = "1";
+    }, 4000);
+
+};
